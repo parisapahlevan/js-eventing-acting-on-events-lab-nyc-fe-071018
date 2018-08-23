e8ebdc574ad47b994d33efb5f22e3ed8b478561f
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-23 22:17:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const elements = document.getElementById("dodger")
 element.addEventlistener('keydown', (event)=>{
   console.log(event)
 if  (event.which === 37) {
-  
+  event.style.backgroundColor = "yellow"
 }
 })
