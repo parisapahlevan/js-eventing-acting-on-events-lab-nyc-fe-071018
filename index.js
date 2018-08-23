e600e151085b1e7776393284e699c3172daeb333
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-23 22:22:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const elements = document.getElementById("dodger")
 element.addEventlistener('keydown', (event)=>{
   console.log(event)
 if  (event.which === 37) {
-  event.style.backgroundColor = "yellow"
-  let previousLeft = elementevent.style.left = "35px"
+  //"180px"
+  let previousLeft = elementevent.style.left 
+  //180
+  let previousLeftNumber = parseInt(previousLeft.replace("px,"), 10)
 }
 })
