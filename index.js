71789521bb3a7604e118e64d1e9f941e1da76432
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-23 22:20:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ element.addEventlistener('keydown', (event)=>{
   console.log(event)
 if  (event.which === 37) {
   event.style.backgroundColor = "yellow"
-  event.style.left = "35px"
+  let previousLeft = elementevent.style.left = "35px"
 }
 })
