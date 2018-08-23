6501be84a345bd57324f9d32a4658e4dacacd6e8
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-23 22:42:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ if(event,which ===37){
 function moveDodgerRight(event) {
   if (event.which === 39) {
     //"180px"
-  let previousLeft = element.style.left
+  let previousRight = element.style.left
   
   //180
   let previousRightNumber = parseInt(previousRight.replace("px,"), 10)
