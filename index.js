166f269e230ad333e15cd117e83c613280b82400
parisa pahlevan <paris.pahlevan@gmail.com>
2018-08-23 22:26:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if  (event.which === 37) {
   let previousLeft = elementevent.style.left 
   //180
   let previousLeftNumber = parseInt(previousLeft.replace("px,"), 10)
-  
   let newLeftNumber = (previousLeftNumber-1) + "px"
+  element.style.left = newLeftNumber
 }
 })
